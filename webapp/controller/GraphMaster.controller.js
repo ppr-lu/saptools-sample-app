9f99bb9a45147f2550bc4b0cd61104a48654a35d
Pablo <pablo.penna@libeup.com>
2019-07-02 09:18:53 +0200

    Day 11: > All the implementation of GraphMaster that retrieved information of a single graph has been moved to GraphDetail (WIP, only the view). > The implementation of graphMaster until noew has been modified to retrieve information of several graphs from the transaction TX_LISTADO_PAGINAS instead of TX_GET_GRAFICA

--- webapp/controller/GraphMaster.controller.js
+++ webapp/controller/GraphMaster.controller.js
@@ -6,17 +6,21 @@ sap.ui.define([
     "sap/ui/unified/Calendar",
     "sap/m/MessageBox",
     "sap/ui/core/routing/History",
+    "sap/viz/ui5/format/ChartFormatter",
     "simple-app/utils/util",
     "simple-app/utils/formatter",
-], function(Controller, JSONModel, Filter, FilterOperator, Calendar, MessageBox, History, Util, Formatter) {
+], function(Controller, JSONModel, Filter, FilterOperator, Calendar, MessageBox, History, ChartFormatter, Util, Formatter) {
 	"use strict";
 
 	return Controller.extend("simple-app.controller.GraphMaster", {
 
         formatter: Formatter,
+
+        formatPatter: ChartFormatter.DefaultPattern,
         
         onInit: function(){
             console.log("onInit graph");
+
         },
         
         onNavBack: function(){
@@ -32,7 +36,27 @@ sap.ui.define([
         },
 
         onRetrieveGraphData: function(oEvent){
-            this.retrieveGraphData();
+            //this.retrieveGraphData();
+            this.testGeneralData();
+        },
+
+        testGeneralData: function(params){
+            var that = this;
+            if(params === undefined){
+                params = Util.getMasterGraphParams();
+                //params = Util.getDetailGraphParams({id_grafica: "41"});
+            }
+            var settings = {
+                
+                url: "http://desarrollos.lyrsa.es/XMII/SOAPRunner/MEFRAGSA/Fundicion/Pantallas/TX_LISTADO_PAGINAS",
+                //url: "http://desarrollos.lyrsa.es/XMII/SOAPRunner/MEFRAGSA/Fundicion/Pantallas/Graficas/TX_GET_GRAFICA",
+                httpMethod: "POST",
+                reqParams: params,
+                successCallback: that.bindRetrievedData,
+                //completeCallback: function(){},
+                //errorCallback: function(){}
+            }
+            Util.sendSOAPRequest(settings, that);
         },
 
         retrieveGraphData: function(params){
@@ -46,8 +70,9 @@ sap.ui.define([
                 url: "http://desarrollos.lyrsa.es/XMII/SOAPRunner/MEFRAGSA/Fundicion/Pantallas/Graficas/TX_GET_GRAFICA",
                 httpMethod: "POST",
                 reqParams: params,
-                successCallback: that.bindRetrievedData
-                //completeCallback: that._unsetGeneralTableBusy //Busyness is unset inside bindRetrieveData
+                successCallback: that.bindRetrievedData,
+                completeCallback: function(){},
+                errorCallback: function(){}
             }
             //that._setGeneralTableBusy();
             Util.sendSOAPRequest(settings, that);  
@@ -60,20 +85,106 @@ sap.ui.define([
             var oDataXML = Util.unescapeXML(data)
             var oDataJSON = Util.xmlToJson(oDataXML);
             //Remove unneeded path prefix
-            oDataJSON = oDataJSON["soap:Envelope"]["soap:Body"].XacuteResponse.Rowset.Row.O_GRAFICA.GRAFICA;
-            oDataJSON = controllerInstance._turn2RealDates(oDataJSON);
+            oDataJSON = oDataJSON["soap:Envelope"]["soap:Body"].XacuteResponse.Rowset.Row.O_GRAFICAS.GRUPOS; //Master
+            //oDataJSON = oDataJSON["soap:Envelope"]["soap:Body"].XacuteResponse.Rowset.Row.O_GRAFICA.GRAFICA; //Detail
+            //oDataJSON = controllerInstance._turn2RealDates(oDataJSON);
             //PUNTOS: GRAFICA.TITULOS.TITULO[0].VARIABLES.VARIABLE[0].PUNTOS
             var oJSONModel = new JSONModel(oDataJSON);
 
             oView.setModel(oJSONModel, "modmastergraph");
+
+            //Config tooltip
+            var oVizFrame = controllerInstance.getView().byId("vizFrame2");
+            var oTooltip = new sap.viz.ui5.controls.VizTooltip({});
+            oTooltip.connect(oVizFrame.getVizUid());
+            oTooltip.setFormatString(ChartFormatter.DefaultPattern.STANDARDFLOAT);
+
+            //Dinamically generate graphs
+
+            var oSettings = {
+                dimensions : [
+                    {axis: 1, name: "Date", value: "{FECHA_TAG/#text}"},
+                    
+                ],
+                measures : [
+                    {group: 1, name : "Production", value : "{VALOR/#text}"},
+                    {group: 1, name : "Second", value : "123"},
+                    
+                ],
+                data : {
+                    path : "modmastergraph>/GRUPO/0/GRAFICAS/GRAFICA/0/PUNTOS/punto/",
+                }
+              };
+
+            var oDataset = new sap.viz.core.FlattenedDataset(oSettings);
+
+            var oChart1 = new sap.viz.ui5.Line(
+                //"idChar1", 
+                {
+                    width : "80%",
+                    height : "400px",
+                    title: {
+                        visible : true,
+                        text : "JS Generated"
+                    },                
+                    plotArea:{                                        
+                        marker: {
+                            visible:false                    		
+                        }                                        
+                    },
+                    dataset: oDataset               
+                }
+            );
+
+            var container = controllerInstance.byId("GraphContainer");
+            container.addContent(oChart1);
+
+            controllerInstance._mergeGraphData();
         },
 
         _turn2RealDates: function(oData){
             var aPoints = oData.TITULOS.TITULO.VARIABLES.VARIABLE.PUNTOS.puntos;
             for(var pt of aPoints){
-                pt.FECHA_TAG["#text"] = new Date(pt.FECHA_TAG["#text"]);
+                pt.VALOR = parseFloat(pt.VALOR["#text"]);
+                pt.FECHA_TAG = new Date(pt.FECHA_TAG["#text"]);
             }
             return oData;
-        }
+        },
+
+        _mergeGraphData: function(){
+            var oView = this.getView();
+            var oOriginalModel = oView.getModel("modmastergraph");
+            var oOriginalData = oOriginalModel.getData();
+
+            var oCopiedData = JSON.parse( JSON.stringify(oOriginalData) );
+
+            var oNewData = {};
+            var aGraphTraces = [];
+            //Iterador sobre grupos - cada grupo contiene varias graficas
+            for(var gr_ix=0;gr_ix<oCopiedData.GRUPO.length;gr_ix++){
+
+                let trace_arr = oCopiedData.GRUPO[gr_ix].GRAFICAS.GRAFICA
+                if(trace_arr != null && !Array.isArray(trace_arr)){
+                    //CAUTION MAI FREN
+                    trace_arr = [trace_arr];
+                }
+                //Iterador sobre gráficas - cada gráfica contiene uno o varios grupos de puntos (trazas)
+                for(var tr_ix=0;tr_ix<trace_arr.length;tr_ix++){
+                    let points_arr = trace_arr[tr_ix].PUNTOS.punto;
+                    if(points_arr != null && !Array.isArray(points_arr)){
+                        //CAUTION MAI FREN
+                        points_arr = [points_arr];
+                    }
+                    //Iterador sobre puntos
+                    for(var pt_ix=0;pt_ix<points_arr.length;pt_ix++){
+
+                    }
+                }
+
+            }
+
+
+
+        },
 	});
 });
