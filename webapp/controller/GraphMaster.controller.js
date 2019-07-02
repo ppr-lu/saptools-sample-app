173e92d943baf29c2fbb989d5b16a3df58802479
Pablo <pablo.penna@libeup.com>
2019-07-02 14:39:42 +0200

    Day 12: > Graphs in master view are now retrieved dinamically. Very RAW presentation.

--- webapp/controller/GraphMaster.controller.js
+++ webapp/controller/GraphMaster.controller.js
@@ -93,18 +93,130 @@ sap.ui.define([
 
             oView.setModel(oJSONModel, "modmastergraph");
 
-            //Config tooltip
-            var oVizFrame = controllerInstance.getView().byId("vizFrame2");
-            var oTooltip = new sap.viz.ui5.controls.VizTooltip({});
-            oTooltip.connect(oVizFrame.getVizUid());
-            oTooltip.setFormatString(ChartFormatter.DefaultPattern.STANDARDFLOAT);
+            controllerInstance._genModifiedGraphData();
+            controllerInstance.genVizChartsFromGraphData("FECHA_TAG", "VALOR");
+            
+        },
 
+        _turn2RealDates: function(oData){
+            var aPoints = oData.TITULOS.TITULO.VARIABLES.VARIABLE.PUNTOS.puntos;
+            for(var pt of aPoints){
+                pt.VALOR = parseFloat(pt.VALOR["#text"]);
+                pt.FECHA_TAG = new Date(pt.FECHA_TAG["#text"]);
+            }
+            return oData;
+        },
+
+        /**
+         * Receive the x and y labels the original graph data has for its points.
+         * 
+         * Modifies the original graph data to store data in a different way so that it can
+         * be integrated with viz charts. Stores data of the different traces in the same place. They
+         * are  differentiated by its label.
+         */
+        _genModifiedGraphData: function(xLabel, yLabel){
+            if(xLabel === undefined){
+                xLabel = "FECHA_TAG";
+            }
+            if(yLabel === undefined){
+                yLabel = "VALOR";
+            }
+            var oView = this.getView();
+            var oOriginalModel = oView.getModel("modmastergraph");
+            var oOriginalData = oOriginalModel.getData();
+
+            var oCopiedData = JSON.parse( JSON.stringify(oOriginalData) );
+
+            var oNewData = {GRAPHS: []};
+            var newGrp;
+            //Iterador sobre grupos - cada grupo contiene varias graficas
+            for(var gr_ix=0;gr_ix<oCopiedData.GRUPO.length;gr_ix++){
+                var currentGrp = oCopiedData.GRUPO[gr_ix];
+                newGrp = {};
+                newGrp.ID = currentGrp.ID["#text"];
+                newGrp.NOMBRE = currentGrp.NOMBRE["#text"];
+                //Dict - key: label, value: label_descr
+                newGrp.DESCRIPCIONES = {};
+                //Contains a list of all the labels used in the traces (labels of y axis)
+                newGrp.ETIQUETAS = [];
+                //Unidades de las trazas
+                newGrp.UNIDADES = [];
+                //stores all points
+                newGrp.TRAZAS = [];
+
+                //force array
+                let trace_arr = currentGrp.GRAFICAS.GRAFICA
+                if(trace_arr != null && !Array.isArray(trace_arr)){
+                    //CAUTION MAI FREN
+                    trace_arr = [trace_arr];
+                }
+                //Iterador sobre gráficas - cada gráfica contiene uno o varios grupos de puntos (trazas)
+                for(var tr_ix=0;trace_arr!==undefined && tr_ix<trace_arr.length;tr_ix++){
+                    var currentGraph = trace_arr[tr_ix];
+                    //store description for the label
+                    newGrp.DESCRIPCIONES[currentGraph.NOMBRE["#text"]] = currentGraph.CARACTERISTICA["#text"];
+                    newGrp.UNIDADES.push(currentGraph.UNIDAD["#text"]);
+                    var currentYLabel = currentGraph.NOMBRE["#text"];
+                    newGrp.ETIQUETAS.push(currentYLabel);
+
+                    //force array
+                    let points_arr = trace_arr[tr_ix].PUNTOS.punto;
+                    if(points_arr != null && !Array.isArray(points_arr)){
+                        //CAUTION MAI FREN
+                        points_arr = [points_arr];
+                    }
+                    //Iterador sobre puntos
+                    for(var pt_ix=0;points_arr!==undefined && pt_ix<points_arr.length;pt_ix++){
+                        var currentPnt = points_arr[pt_ix];
+                        if(newGrp.TRAZAS[pt_ix] === undefined){
+                            newGrp.TRAZAS[pt_ix] = {};
+                            //newGrp.TRAZAS[pt_ix]["ETIQUETA_EJEX"] = currentPnt[xLabel];
+                            newGrp.TRAZAS[pt_ix][xLabel] = currentPnt[xLabel]["#text"];
+                            newGrp.TRAZAS[pt_ix][currentYLabel] = currentPnt[yLabel]["#text"];
+                        }else{
+                            newGrp.TRAZAS[pt_ix][currentYLabel] = currentPnt[yLabel]["#text"];
+                        }
+                    }
+                }
+                oNewData.GRAPHS.push(newGrp);
+            }
+            oView.setModel(new JSONModel(oNewData), "modmodifiedmastergraph");
+        },
+
+        /**From the modified data of the graphs, generate needed data to create them and
+         * call the function that draws them.
+         */
+        genVizChartsFromGraphData: function(xLabel, yLabel, container){
+            container = container || "GraphContainer";
+            xLabel = xLabel || "FECHA_TAG";
+            yLabel = yLabel || "VALOR";
+            var oView = this.getView();
+            var oModel = oView.getModel("modmodifiedmastergraph");
+            var oData = oModel.getData();
+
+            for(var gr_ix=0;gr_ix<oData.GRAPHS.length;gr_ix++){
+                var currentGraph = oData.GRAPHS[gr_ix];
+                var aEtiquetas = currentGraph.ETIQUETAS;
+                var aDimensions = [];
+                var aMeasures = [];
+
+                aDimensions.push({axis: 1, name: xLabel, value: "{"+xLabel+"}"});
+                for(var label of aEtiquetas){
+                    aMeasures.push({group: 1, name: label, value:"{"+label+"}"});
+                }
+                var sDataPath = "modmodifiedmastergraph>/GRAPHS/"+gr_ix+"/TRAZAS/";
+
+                this._generateVizChart(container, aDimensions, aMeasures, sDataPath);
+            }
+        },
+
+        /**Creates a Viz Chart using the data received*/
+        _generateVizChart: function(containerId, aDimensions, aMeasures, sDataPath){
+            var oSettings;
             //Dinamically generate graphs
-
-            var oSettings = {
+            oSettings = {
                 dimensions : [
-                    {axis: 1, name: "Date", value: "{FECHA_TAG/#text}"},
-                    
+                    {axis: 1, name: "Date", value: "{FECHA_TAG/#text}"}, 
                 ],
                 measures : [
                     {group: 1, name : "Production", value : "{VALOR/#text}"},
@@ -114,11 +226,19 @@ sap.ui.define([
                 data : {
                     path : "modmastergraph>/GRUPO/0/GRAFICAS/GRAFICA/0/PUNTOS/punto/",
                 }
-              };
+            };
+
+            oSettings = {
+                dimensions : aDimensions,
+                measures : aMeasures,
+                data : {
+                    path : sDataPath,
+                }
+            };
 
             var oDataset = new sap.viz.core.FlattenedDataset(oSettings);
 
-            var oChart1 = new sap.viz.ui5.Line(
+            var oChart = new sap.viz.ui5.Line(
                 //"idChar1", 
                 {
                     width : "80%",
@@ -136,55 +256,19 @@ sap.ui.define([
                 }
             );
 
-            var container = controllerInstance.byId("GraphContainer");
-            container.addContent(oChart1);
-
-            controllerInstance._mergeGraphData();
-        },
-
-        _turn2RealDates: function(oData){
-            var aPoints = oData.TITULOS.TITULO.VARIABLES.VARIABLE.PUNTOS.puntos;
-            for(var pt of aPoints){
-                pt.VALOR = parseFloat(pt.VALOR["#text"]);
-                pt.FECHA_TAG = new Date(pt.FECHA_TAG["#text"]);
+            var container = this.byId(containerId);
+            if(container){
+                container.addContent(oChart);
             }
-            return oData;
         },
 
-        _mergeGraphData: function(){
-            var oView = this.getView();
-            var oOriginalModel = oView.getModel("modmastergraph");
-            var oOriginalData = oOriginalModel.getData();
-
-            var oCopiedData = JSON.parse( JSON.stringify(oOriginalData) );
-
-            var oNewData = {};
-            var aGraphTraces = [];
-            //Iterador sobre grupos - cada grupo contiene varias graficas
-            for(var gr_ix=0;gr_ix<oCopiedData.GRUPO.length;gr_ix++){
-
-                let trace_arr = oCopiedData.GRUPO[gr_ix].GRAFICAS.GRAFICA
-                if(trace_arr != null && !Array.isArray(trace_arr)){
-                    //CAUTION MAI FREN
-                    trace_arr = [trace_arr];
-                }
-                //Iterador sobre gráficas - cada gráfica contiene uno o varios grupos de puntos (trazas)
-                for(var tr_ix=0;tr_ix<trace_arr.length;tr_ix++){
-                    let points_arr = trace_arr[tr_ix].PUNTOS.punto;
-                    if(points_arr != null && !Array.isArray(points_arr)){
-                        //CAUTION MAI FREN
-                        points_arr = [points_arr];
-                    }
-                    //Iterador sobre puntos
-                    for(var pt_ix=0;pt_ix<points_arr.length;pt_ix++){
-
-                    }
-                }
-
-            }
-
-
-
+        _addTooltip2VizFrame: function(oVizFrame){
+            //Config tooltip
+            var oVizFrame = controllerInstance.getView().byId("vizFrame2");
+            var oTooltip = new sap.viz.ui5.controls.VizTooltip({});
+            oTooltip.connect(oVizFrame.getVizUid());
+            //oTooltip.setFormatString(ChartFormatter.DefaultPattern.STANDARDFLOAT);
         },
+
 	});
 });
