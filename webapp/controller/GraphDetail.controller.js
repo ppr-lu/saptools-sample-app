7e13a7c44ea4a978242415335daf6bc291fcc271
Pablo <pablo.penna@libeup.com>
2019-07-09 09:12:57 +0200

    Day 16: > Finished detail graph View. It loads mock data. Check commented functions to make it retrieve data from the server instead.

--- webapp/controller/GraphDetail.controller.js
+++ webapp/controller/GraphDetail.controller.js
@@ -7,9 +7,11 @@ sap.ui.define([
     "sap/viz/ui5/controls/common/feeds/FeedItem",
     "sap/viz/ui5/format/ChartFormatter",
     "sap/viz/ui5/data/FlattenedDataset",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
     "simple-app/utils/util",
     "simple-app/utils/formatter",
-], function(Controller, JSONModel, MessageBox, History, VizFrame, FeedItem, ChartFormatter, FlattenedDataset, Util, Formatter) {
+], function(Controller, JSONModel, MessageBox, History, VizFrame, FeedItem, ChartFormatter, FlattenedDataset, Filter, FilterOperator, Util, Formatter) {
 	"use strict";
 
 	return Controller.extend("simple-app.controller.GraphMaster", {
@@ -22,7 +24,10 @@ sap.ui.define([
             console.log("onInit graphDetail");
 
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.getRoute("graphDetail").attachPatternMatched(this._onObjectMatched, this);
+            oRouter.getRoute("graphDetail").attachPatternMatched(this._onObjectMatched, this);
+            
+            this._initSliderModel();
+            this._configureChartSlider();
 
         },
 
@@ -30,7 +35,8 @@ sap.ui.define([
             var graphId = oEvent.getParameter("arguments").graphId;
             console.log("graph id:"  + graphId);
 
-            this.retrieveGraphData(graphId);
+            // this.retrieveGraphData(graphId);    //Retrieve data from server
+            this._genGraphData();               //Generate data locally
 		},
         
         onNavBack: function(){
@@ -76,8 +82,10 @@ sap.ui.define([
             var oJSONModel = new JSONModel(oDataJSON);
 
             oView.setModel(oJSONModel, "moddetailgraph");
-            //controllerInstance._genModifiedGraphData();
-            //controllerInstance.genVizChartFromGraphData("FECHA_TAG", "VALOR");
+
+            //PATCH - Set title
+            //REVISE PATH OF THE TITLE (oDataJSON.TITULOS.TITULO.TEXTO)
+            controllerInstance._setGraphTitle(oView.byId("idVizFrame"), oDataJSON.TITULOS.TITULO.TEXTO);
             
         },
 
@@ -92,233 +100,178 @@ sap.ui.define([
         },
 
         _onGraphRenderComplete: function(oEvent){
-            debugger;
             this._addTooltip2VizFrame(oEvent.getSource());
-        },
-
-        /**2BEUPDATED
-         * Receive the x and y labels the original graph data has for its points.
-         * 
-         * Modifies the original graph data to store data in a different way so that it can
-         * be integrated with viz charts. Stores data of the different traces in the same place. They
-         * are  differentiated by its label.
-         */
-        _genModifiedGraphData: function(xLabel, yLabel){
-            if(xLabel === undefined){
-                xLabel = "FECHA_TAG";
-            }
-            if(yLabel === undefined){
-                yLabel = "VALOR";
-            }
-            var oView = this.getView();
-            var oOriginalModel = oView.getModel("moddetailgraph");
-            var oOriginalData = oOriginalModel.getData();
-
-            var oCopiedData = JSON.parse( JSON.stringify(oOriginalData) );
-
-            var oNewData = {GRAPHS: []};
-            var newGrp;
-            //Iterador sobre grupos - cada grupo contiene varias graficas
-            for(var gr_ix=0;gr_ix<oCopiedData.GRUPO.length;gr_ix++){
-                var currentGrp = oCopiedData.GRUPO[gr_ix];
-                newGrp = {};
-                newGrp.ID = currentGrp.ID["#text"];
-                newGrp.NOMBRE = currentGrp.NOMBRE["#text"];
-                //Dict - key: label, value: label_descr
-                newGrp.DESCRIPCIONES = {};
-                //Contains a list of all the labels used in the traces (labels of y axis)
-                newGrp.ETIQUETAS = [];
-                //Unidades de las trazas
-                newGrp.UNIDADES = [];
-                //stores all points
-                newGrp.TRAZAS = [];
-
-                //force array
-                let trace_arr = currentGrp.GRAFICAS.GRAFICA
-                if(trace_arr != null && !Array.isArray(trace_arr)){
-                    //CAUTION MAI FREN
-                    trace_arr = [trace_arr];
-                }
-                //Iterador sobre gráficas - cada gráfica contiene uno o varios grupos de puntos (trazas)
-                for(var tr_ix=0;trace_arr!==undefined && tr_ix<trace_arr.length;tr_ix++){
-                    var currentGraph = trace_arr[tr_ix];
-                    //store description for the label
-                    newGrp.DESCRIPCIONES[currentGraph.NOMBRE["#text"]] = currentGraph.CARACTERISTICA["#text"];
-                    newGrp.UNIDADES.push(currentGraph.UNIDAD["#text"]);
-                    var currentYLabel = currentGraph.NOMBRE["#text"];
-                    newGrp.ETIQUETAS.push(currentYLabel);
-
-                    //force array
-                    let points_arr = trace_arr[tr_ix].PUNTOS.punto;
-                    if(points_arr != null && !Array.isArray(points_arr)){
-                        //CAUTION MAI FREN
-                        points_arr = [points_arr];
-                    }
-                    //Iterador sobre puntos
-                    for(var pt_ix=0;points_arr!==undefined && pt_ix<points_arr.length;pt_ix++){
-                        var currentPnt = points_arr[pt_ix];
-                        if(newGrp.TRAZAS[pt_ix] === undefined){
-                            newGrp.TRAZAS[pt_ix] = {};
-                            //newGrp.TRAZAS[pt_ix]["ETIQUETA_EJEX"] = currentPnt[xLabel];
-                            newGrp.TRAZAS[pt_ix][xLabel] = currentPnt[xLabel]["#text"];
-                        }
-                        newGrp.TRAZAS[pt_ix][currentYLabel] = parseFloat(currentPnt[yLabel]["#text"]).toFixed(3) || currentPnt[yLabel]["#text"];
-                        
-                    }
-                }
-                oNewData.GRAPHS.push(newGrp);
-            }
-            oView.setModel(new JSONModel(oNewData), "modmodifiedmastergraph");
-        },
-
-        /**2BEUPDATED
-         * From the modified data of the graphs, generate needed data to create them and
-         * call the function that draws them.
-         */
-        genVizChartFromGraphData: function(xLabel, yLabel, container){
-            container = container || "GraphContainer";
-            xLabel = xLabel || "FECHA_TAG";
-            yLabel = yLabel || "VALOR";
-            var oView = this.getView();
-            var oModel = oView.getModel("modmodifiedmastergraph");
-            var oData = oModel.getData();
-
-            for(var gr_ix=0;gr_ix<oData.GRAPHS.length;gr_ix++){
-                var currentGraph = oData.GRAPHS[gr_ix];
-                var aEtiquetas = currentGraph.ETIQUETAS;
-                var aDimensions;
-                var aMeasures;
-                var sDataPath = "modmodifiedmastergraph>/GRAPHS/"+gr_ix+"/TRAZAS/";
-                
-                for(var label of aEtiquetas){
-                    aDimensions = [];
-                    aMeasures = [];
-                    aDimensions.push({axis: 1, name: xLabel, value: "{"+xLabel+"}"});
-                    aMeasures.push({group: 1, name: label, value:"{"+label+"}"});
-                    //this._generateVizChart(container, aDimensions, aMeasures, sDataPath);
-                    this._generateVizFrame(container, aDimensions, aMeasures, sDataPath);
-                }
-            }
-        },
-
-        /**DEPRECATED LIB - Creates a Viz Chart using the data received*/
-        _generateVizChart: function(containerId, aDimensions, aMeasures, sDataPath){
-            var oSettings;
-            //Dinamically generate graphs
-            oSettings = {
-                dimensions : aDimensions,
-                measures : aMeasures,
-                data : {
-                    path : sDataPath,
-                }
-            };
-
-            var oDataset = new FlattenedDataset(oSettings);
-
-            //var oChart = new sap.viz.ui5.Line(
-            var oChart = new sap.viz.ui5.Area(
-                //"idChar1", 
-                {
-                    width : "80%",
-                    height : "400px",
-                    title: {
-                        visible : true,
-                        text : "JS Generated"
-                    },                
-                    plotArea:{                                        
-                        marker: {
-                            visible:false                    		
-                        }                                        
-                    },
-                    dataset: oDataset               
-                }
-            );
-
-            var vbox = new sap.m.VBox();
-            vbox.addItem(oChart);    
-            vbox.addItem(new sap.m.Button({text: "DETALLE"}));   
-            var container = this.byId(containerId);
-            if(container){
-                container.addContent(vbox);
-            }
-        },
-
-        /**UP TO DATE LIB - Creates a Viz Frame using the data received*/
-        _generateVizFrame: function(containerId, aDimensions, aMeasures, sDataPath){
-            
-            var oView = this.getView();
-            var oModel = oView.getModel("modmodifiedmastergraph");
-
-            var oSettings = {
-                dimensions : aDimensions,
-                measures : aMeasures,
-                data : {
-                    path : sDataPath,
-                }
-            };
-
-            var oDataset = new FlattenedDataset(oSettings);
-
-            var oFrame = new VizFrame(
-                {
-                    width : "80%",
-                    height : "400px",
-                }
-            );
-
-            oFrame.setDataset(oDataset);
-            oFrame.setModel(oModel);
-            oFrame.setVizType('area');
-
-            // 4.Set Viz properties
-            oFrame.setVizProperties({
-                plotArea: {
-                    colorPalette : d3.scale.category20().range()
-                }
-            });
-
-            //Measure Feed Items (y axis)
-            var aValues = []
-            for(var i=0;i<aMeasures.length;i++){
-                aValues.push(aMeasures[i].name);
-            }
-            var feedValueAxis = new FeedItem({
-                'uid': "valueAxis",
-                'type': "Measure",
-                'values': aValues
-            });
-            oFrame.addFeed(feedValueAxis);
-
-            //Dimension Feed Items (x axis)
-            aValues=[];
-            for(var i=0;i<aDimensions.length;i++){
-                aValues.push(aDimensions[i].name);
-            }
-            feedCategoryAxis = new FeedItem({
-                'uid': "categoryAxis",
-                'type': "Dimension",
-                'values': aValues
-            });
-            oFrame.addFeed(feedCategoryAxis);
-
-            //----
-
-            var vbox = new sap.m.VBox();
-            vbox.addItem(oChart);    
-            vbox.addItem(new sap.m.Button({text: "DETALLE"}));   
-            var container = this.byId(containerId);
-            if(container){
-                container.addContent(vbox);
-            }
         },
 
         _addTooltip2VizFrame: function(oVizFrame){
             //Config tooltip
             //var oVizFrame = controllerInstance.getView().byId("vizFrame2");
-            var oTooltip = new sap.viz.ui5.controls.VizTooltip({});
+            // var oFormat = {};
+
+            var oTooltipConfig = {
+                formatString: {
+                    Data: '0.00',
+                    Date:  'HH:mm - DD/MM/YYYY'
+                } 
+            };
+            var oTooltip = new sap.viz.ui5.controls.VizTooltip(oTooltipConfig);
             oTooltip.connect(oVizFrame.getVizUid());
-            //oTooltip.setFormatString(ChartFormatter.DefaultPattern.STANDARDFLOAT);
         },
+
+        /**PATCH - Does not work otherwise
+         * https://stackoverflow.com/questions/40301442/how-to-set-a-title-for-vizframe-chart */
+        _setGraphTitle: function(oGraph, sTitle){
+            var asyncChartUpdate = function () {
+                oGraph.setVizProperties({
+                    title: {
+                        text: sTitle
+                    },
+                    categoryAxis: {
+                        labelRenderer: jQuery.proxy(this.testFunction, this)
+                    }
+                });
+            };
+            setTimeout(asyncChartUpdate, 0);
+        },
+
+        
+        /***
+         *     _____ _ _     _           
+         *    /  ___| (_)   | |          
+         *    \ `--.| |_  __| | ___ _ __ 
+         *     `--. \ | |/ _` |/ _ \ '__|
+         *    /\__/ / | | (_| |  __/ |   
+         *    \____/|_|_|\__,_|\___|_|   
+         *                               
+         *                               
+         */
+
+        _initSliderModel: function(){
+            var oView = this.getView();
+            var oData = {
+                fechadesde: "",
+                fechahasta: ""
+            }
+            oView.setModel(new JSONModel(oData), "modsliderrange");
+        },
+
+        _configureChartSlider: function(){
+            var oView = this.getView();
+            var oModel = oView.getModel("moddetailgraph");
+            var oVizFrame = oView.byId("idVizFrame");
+            var oRangeSlider = oView.byId("idVizSlider");
+            oRangeSlider.setModel(oModel);
+            oRangeSlider.setValueAxisVisible(false);
+            oRangeSlider.setShowPercentageLabel(false);
+            // oRangeSlider.setShowStartEndLabel(false);
+            oRangeSlider.setLayoutData(new sap.m.FlexItemData({
+                maxHeight: '10%',
+                baseSize: '100%',
+                order: 1,
+                styleClass: 'rangeSliderPadding'
+            }));
+
+            oRangeSlider.attachRangeChanged(function(e){
+                var data = e.getParameters().data;
+                var start = data.start.Date;
+                var end = data.end.Date;
+                var dateFilter =  new Filter({
+                    path: "FECHA_TAG",
+                    test: function(oValue) {
+                        var time = Date.parse(new Date(oValue));
+                        return (time >= start && time <= end);
+                    }
+                });
+                oVizFrame.getDataset().getBinding('data').filter([dateFilter]);
+            });
+        },
+
+        _setChartSliderRange: function(oChartSlider, startDateMs, endDateMs){
+            var range = {
+                'start' : startDateMs,
+                'end' : endDateMs
+            };
+            oChartSlider.setRange(range);
+            //fire range change event
+            //Give params with the data and structure expected
+            var param = {
+                data: {
+                    start: {Date: range.start},
+                    end: {Date: range.end}
+                }
+            };
+            oChartSlider.fireRangeChanged(param);
+        },
+
+        onApplySliderRange: function(oEvent){
+            var oView = this.getView();
+            var oRangeModel = oView.getModel("modsliderrange");
+            var fechadesde = new Date(oRangeModel.getProperty("/fechadesde")).getTime();
+            var fechahasta = new Date(oRangeModel.getProperty("/fechahasta")).getTime();
+            var oSlider = oView.byId("idVizSlider");
+
+            this._setChartSliderRange(oSlider, fechadesde, fechahasta);
+        },
+
+        onResetSliderRange: function(oEvent){
+            var oView = this.getView();
+            //deatetime pickers
+            var oRangeModel = oView.getModel("modsliderrange");
+            oRangeModel.setProperty("/fechadesde", "");
+            oRangeModel.setProperty("/fechahasta", "");
+            //Slider
+            var oSlider = oView.byId("idVizSlider");
+            var fechadesde = 0
+            var fechahasta = new Date("9999/12/31").getTime();
+
+            this._setChartSliderRange(oSlider, fechadesde, fechahasta);
+        },
+        
+        /***
+         *     _____ _____ _____ _____ _____ _   _ _____ 
+         *    |_   _|  ___/  ___|_   _|_   _| \ | |  __ \
+         *      | | | |__ \ `--.  | |   | | |  \| | |  \/
+         *      | | |  __| `--. \ | |   | | | . ` | | __ 
+         *      | | | |___/\__/ / | |  _| |_| |\  | |_\ \
+         *      \_/ \____/\____/  \_/  \___/\_| \_/\____/
+         *                                               
+         *                                               
+         */
+
+        _genRandomPoints: function(numPts){
+            numPts = numPts || 1000;
+            var aRes = [];
+            var rndObj = {FECHA_TAG: new Date(), VALOR: ""};
+            for(var i=0;i<numPts;i++){
+                rndObj.VALOR = parseFloat(700 + 300*Math.random()).toFixed(3);
+                rndObj.VALOR2 = parseInt(10 + 20*Math.random());
+                rndObj.FECHA_TAG = new Date(rndObj.FECHA_TAG.getTime() + 600000); //10 min
+                //rndObj.FECHA_TAG = new Date(rndObj.FECHA_TAG.getTime() + 86400000); //1 day
+                let aNewObj = JSON.parse( JSON.stringify(rndObj) );
+                aRes.push(aNewObj); 
+            }
+            return aRes;
+        },
+
+        _formatDate: function(d){
+            var sDate = d.getHour()+":"+d.getMinutes()+" "+d.getFullYear()+"/"+parseInt(d.getMonth()+1,10)+"/"+d.getDate();
+            return sDate;
+        },
+
+        _genGraphData: function(){
+            var oView = this.getView();     
+            var data=this._genRandomPoints(100);
+            // /TITULOS/TITULO/VARIABLES/VARIABLE/PUNTOS/puntos/
+            var oDataJSON = {TITULOS: {TITULO: {TEXTO: "Mock Chart", VARIABLES: {VARIABLE: {PUNTOS: {puntos: data}}}}}};
+            var oJSONModel = new JSONModel(oDataJSON);
+            oView.setModel(oJSONModel, "moddetailgraph");
+
+            this._setGraphTitle(oView.byId("idVizFrame"), oDataJSON.TITULOS.TITULO.TEXTO);
+        },
+
+        testFunction: function(rendData){
+            debugger;
+        }
 
 	});
 });
